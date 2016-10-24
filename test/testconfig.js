8279f0bedd24c0e3db1b4073d4f43094b85005b7
Dmitry Chertousov <d.chertousov@gmail.com>
2016-10-24 17:53:19 +0200

    Test config updated

--- test/testconfig.js
+++ test/testconfig.js
@@ -1,13 +1,6 @@
-if (!process.env.USERNAME) {
-  throw new Error('USERNAME env var should be provided');
-}
-if (!process.env.PASSWORD) {
-  throw new Error('PASSWORD env var should be provided');
-}
-
 module.exports = {
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  targetBranch: process.env.BRANCH,
-  pcc: process.env.PCC,
+  username: 'USERNAME',
+  password: 'PASSWORD',
+  targetBranch: 'BRANCH',
+  pcc: 'PCC',
 };
