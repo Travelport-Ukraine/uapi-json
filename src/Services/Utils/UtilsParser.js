f561d1bc4b949c54fc80d71bce0891362bc02cf1
Dmitry Chertousov <d.chertousov@gmail.com>
2016-10-24 09:38:38 +0200

    Moved UtilsError handler into Parser

--- src/Services/Utils/UtilsParser.js
+++ src/Services/Utils/UtilsParser.js
@@ -14,6 +14,21 @@ function currencyConvertParse(json) {
   return json;
 }
 
+const errorHandler = (err) => {
+  let errno = 0;
+  try {
+    errno = err['SOAP:Fault'][0].detail[0]['common_v34_0:ErrorInfo'][0]['common_v34_0:Code'][0];
+  } catch (e) {
+    console.log('cant parse error');
+  }
+
+  switch (errno * 1) {
+    default:
+      throw new UError('UNHANDLED_ERROR', err);
+  }
+};
+
 module.exports = {
+  UTILS_ERROR: errorHandler,
   CURRENCY_CONVERSION: currencyConvertParse,
 };
