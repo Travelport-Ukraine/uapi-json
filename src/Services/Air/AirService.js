5303dad3e10fae99d6d26ae5d33a32c48fc8acb5
Mark <mail.ormark@gmail.com>
2016-10-27 17:40:15 +0300

    For correct work we should add some time to pass validation in travelport.

--- src/Services/Air/AirService.js
+++ src/Services/Air/AirService.js
@@ -24,7 +24,7 @@ module.exports = (settings) => {
           passengers: options.passengers,
           rule: options.rule,
           ticketingPcc: auth.pcc.toUpperCase(),
-          ticketDate: moment().format(),
+          ticketDate: moment().add(1, 'hours').format(),
           ActionStatusType: 'TAU',
         }, data);
         return AirService.createReservation(bookingParams).catch((err) => {
