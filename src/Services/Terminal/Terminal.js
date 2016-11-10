bc72494b8d01e7ae93ecd1e0618192b570a4d4a2
Dmitry Chertousov <d.chertousov@gmail.com>
2016-11-10 17:22:21 +0100

    Terminal usage enhancements. - Throwing error when terminal is busy. - Auto closing connection on exit whenever possible

--- src/Services/Terminal/Terminal.js
+++ src/Services/Terminal/Terminal.js
@@ -1,8 +1,15 @@
+import util from 'util';
 import { lib as screenLib } from 'galileo-screen';
 import {
   TerminalRuntimeError,
 } from './TerminalErrors';
 import terminalService from './TerminalService';
+
+export const TERMINAL_STATE_NONE = 'TERMINAL_STATE_NONE';
+export const TERMINAL_STATE_BUSY = 'TERMINAL_STATE_BUSY';
+export const TERMINAL_STATE_READY = 'TERMINAL_STATE_READY';
+export const TERMINAL_STATE_CLOSED = 'TERMINAL_STATE_CLOSED';
+export const TERMINAL_STATE_ERROR = 'TERMINAL_STATE_ERROR';
 
 const responseHasMoreData = response => (response.slice(-1).join('') === ')><');
 
@@ -11,8 +18,10 @@ module.exports = function (settings) {
   const emulatePcc = settings.emulatePcc || false;
   const timeout = settings.timeout || false;
   const state = {
+    terminalState: TERMINAL_STATE_NONE,
     sessionToken: null,
   };
+  // Processing response with MD commands
   const processResponse = (response, previousResponse = []) => {
     const processedResponse = screenLib.mergeLastLinesAtIntersection(
       previousResponse.slice(0, -1),
@@ -28,12 +37,19 @@ module.exports = function (settings) {
       mdResponse => processResponse(mdResponse, processedResponse)
     );
   };
+  // Getting session token
   const getSessionToken = () => new Promise((resolve, reject) => {
+    if (state.terminalState === TERMINAL_STATE_BUSY) {
+      throw new TerminalRuntimeError.TerminalIsBusy();
+    }
     // Return token if already obtained
     if (state.sessionToken !== null) {
       resolve(state.sessionToken);
       return;
     }
+    Object.assign(state, {
+      terminalState: TERMINAL_STATE_BUSY,
+    });
     // Getting token
     service.getSessionToken({
       timeout,
@@ -44,27 +60,38 @@ module.exports = function (settings) {
       });
       // Return if no emulation needed
       if (!emulatePcc) {
-        resolve(sessionToken);
-        return;
+        return sessionToken;
       }
       // Emulate pcc
-      service.executeCommand({
+      return service.executeCommand({
         sessionToken,
         command: `SEM/${emulatePcc}/AG`,
       }).then((response) => {
         if (!response[0].match(/^PROCEED/)) {
-          reject(new TerminalRuntimeError.TerminalEmulationFailed(response));
-          return;
+          throw new TerminalRuntimeError.TerminalEmulationFailed(response);
         }
-        resolve(sessionToken);
+        return sessionToken;
       });
-    }).catch(reject);
+    }).then(
+      resolve
+    ).catch((err) => {
+      Object.assign(state, {
+        terminalState: TERMINAL_STATE_ERROR,
+      });
+      reject(err);
+    });
   });
-  return {
+
+  const terminal = {
     executeCommand: command => getSessionToken().then(
       sessionToken => service.executeCommand({
         command,
         sessionToken,
+      }).then((response) => {
+        Object.assign(state, {
+          terminalState: TERMINAL_STATE_READY,
+        });
+        return response;
       }).then(
         response => processResponse(response)
       )
@@ -73,6 +100,50 @@ module.exports = function (settings) {
       sessionToken => service.closeSession({
         sessionToken,
       })
-    ),
+    ).then((response) => {
+      Object.assign(state, {
+        terminalState: TERMINAL_STATE_CLOSED,
+      });
+      return response;
+    }),
   };
+
+  // Adding event handler on beforeExit and exit process events
+  process.on('beforeExit', () => {
+    switch (state.terminalState) {
+      case TERMINAL_STATE_BUSY:
+      case TERMINAL_STATE_READY:
+      case TERMINAL_STATE_ERROR:
+        if (state.terminalState === TERMINAL_STATE_BUSY) {
+          util.log('UAPI-JSON WARNING: Process exited before completing TerminalService request');
+        }
+        util.log('UAPI-JSON WARNING: Process left TerminalService session open');
+        util.log('UAPI-JSON WARNING: Session closing');
+        terminal.closeSession().then(
+          () => util.log('UAPI-JSON WARNING: Session closed')
+        ).catch(
+          () => util.log('UAPI-JSON WARNING: Error closing session')
+        );
+        break;
+      default:
+        break;
+    }
+  });
+  process.on('exit', () => {
+    switch (state.terminalState) {
+      case TERMINAL_STATE_BUSY:
+      case TERMINAL_STATE_READY:
+      case TERMINAL_STATE_ERROR:
+        if (state.terminalState === TERMINAL_STATE_BUSY) {
+          util.log('UAPI-JSON WARNING: Process exited before completing TerminalService request');
+        }
+        util.log('UAPI-JSON WARNING: Process left TerminalService session open');
+        break;
+      default:
+        break;
+    }
+  });
+
+
+  return terminal;
 };
