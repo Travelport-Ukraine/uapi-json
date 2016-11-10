94048f5b844f53bc17a3cf982abe60fa50af6d6d
Dmitry Chertousov <d.chertousov@gmail.com>
2016-11-10 17:25:55 +0100

    Example of TerminalService usage

---
+++ examples/Terminal/ignore.js
@@ -0,0 +1,18 @@
+const uAPI = require('../../index');
+const config = require('../../test/testconfig');
+
+const TerminalService = uAPI.createTerminalService({
+  auth: config,
+  debug: 0,
+  production: true,
+  emulatePcc: '7j8j',
+});
+
+// BAD EXAMPLE OF USAGE
+// Terminal session should be closed
+
+TerminalService.executeCommand('I')
+  .then(console.log)
+  .catch((err) => {
+    console.error(err);
+  });
---
+++ examples/Terminal/serial.js
@@ -0,0 +1,31 @@
+const uAPI = require('../../index');
+const config = require('../../test/testconfig');
+
+const TerminalService = uAPI.createTerminalService({
+  auth: config,
+  debug: 0,
+  production: true,
+  emulatePcc: '7j8j',
+});
+
+TerminalService
+  .executeCommand('I')
+  .then(console.log)
+  .then(
+    () => TerminalService.executeCommand('.CDIEV')
+  )
+  .then(console.log)
+  .then(
+    () => TerminalService.executeCommand('.ADPS')
+  )
+  .then(console.log)
+  .then(
+    () => TerminalService.executeCommand('@LT')
+  )
+  .then(console.log)
+  .then(
+    () => TerminalService.closeSession()
+  )
+  .catch((err) => {
+    console.error(err);
+  });
