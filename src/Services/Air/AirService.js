38eeba58bab6ed87b0e4c2e21952c356ca5e0f8f
Dmitry Chertousov <d.chertousov@gmail.com>
2017-01-19 16:31:01 +0200

    Single service configuration for all methods

--- src/Services/Air/AirService.js
+++ src/Services/Air/AirService.js
@@ -1,40 +1,37 @@
 import moment from 'moment';
 import _ from 'lodash';
-import airServiceInternal from './AirServiceInternal';
+import airService from './AirServiceInternal';
 import { AirRuntimeError } from './AirErrors';
 
 module.exports = (settings) => {
-  const { auth, debug, production } = settings;
+  const service = airService(settings);
   return {
     shop(options) {
-      const AirService = airServiceInternal(auth, debug, production);
-      return AirService.searchLowFares(options);
+      return service.searchLowFares(options);
     },
 
     toQueue(options) {
-      const AirService = airServiceInternal(auth, debug, production);
-      return AirService.gdsQueue(options);
+      return service.gdsQueue(options);
     },
 
     book(options) {
-      const AirService = airServiceInternal(auth, debug, production);
-      return AirService.airPricePricingSolutionXML(options).then((data) => {
+      return service.airPricePricingSolutionXML(options).then((data) => {
         const bookingParams = Object.assign({}, {
           passengers: options.passengers,
           rule: options.rule,
           ticketDate: moment().add(3, 'hours').format(),
           ActionStatusType: 'TAU',
         }, data);
-        return AirService.createReservation(bookingParams).catch((err) => {
+        return service.createReservation(bookingParams).catch((err) => {
           if (err instanceof AirRuntimeError.SegmentBookingFailed
               || err instanceof AirRuntimeError.NoValidFare) {
             const code = err.data['universal:UniversalRecord'].LocatorCode;
-            return AirService.cancelUR({
+            return service.cancelUR({
               LocatorCode: code,
             }).then(() => {
               throw err;
             }).catch(() => {
-              if (debug > 0) {
+              if (settings.debug > 0) {
                 console.log('Cant cancel booking with UR', code);
               }
               throw err;
@@ -46,22 +43,20 @@ module.exports = (settings) => {
     },
 
     importPNR(options) {
-      const AirService = airServiceInternal(auth, debug, production);
-      return AirService.importPNR(options);
+      return service.importPNR(options);
     },
 
     ticket(options) {
-      const AirService = airServiceInternal(auth, debug, production);
-      return AirService.importPNR(options).then((data) => {
+      return service.importPNR(options).then((data) => {
         const ticketParams = Object.assign({}, options, {
           ReservationLocator: data[0].uapi_reservation_locator,
         });
-        return AirService.ticket(ticketParams)
+        return service.ticket(ticketParams)
           .then(result => result, (err) => {
             if (err instanceof AirRuntimeError.TicketingFoidRequired) {
-              return AirService.importPNR(options)
-                .then(booking => AirService.foid(booking[0]))
-                .then(() => AirService.ticket(ticketParams));
+              return service.importPNR(options)
+                .then(booking => service.foid(booking[0]))
+                .then(() => service.ticket(ticketParams));
             }
             return Promise.reject(err);
           });
@@ -72,11 +67,10 @@ module.exports = (settings) => {
       const parameters = {
         flightInfoCriteria: _.isArray(options) ? options : [options],
       };
-      const AirService = airServiceInternal(auth, debug, production);
-      return AirService.flightInfo(parameters)
+      return service.flightInfo(parameters)
         .then(data => data)
         .catch((err) => {
-          if (debug > 0) {
+          if (settings.debug > 0) {
             console.log('Cant get flightInfo', err);
           }
           Promise.reject(err);
