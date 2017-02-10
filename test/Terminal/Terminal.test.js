81473962b2886e5bdb7de9b28fab08a1e2e10fdd
Dmitry Chertousov <d.chertousov@gmail.com>
2017-02-10 12:51:00 +0100

    Added tests for BUSY and CLOSED states, when executing commands

--- test/Terminal/Terminal.test.js
+++ test/Terminal/Terminal.test.js
@@ -15,6 +15,24 @@ const token = 'TOKEN';
 
 // Spied functions
 const getSessionToken = sinon.spy(() => Promise.resolve(token));
+const executeCommandSlow = sinon.spy((params) => {
+  expect(params).to.be.an('object');
+  expect(params.sessionToken).to.equal(token);
+  expect(params.command).to.be.a('string');
+  switch (params.command) {
+    case 'I':
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(['IGNORED']);
+        }, 100);
+      });
+    default:
+      if (params.command.match(/^SEM/)) {
+        return Promise.resolve(['PROCEED']);
+      }
+      return Promise.resolve(['ERR']);
+  }
+});
 const executeCommandOk = sinon.spy((params) => {
   expect(params).to.be.an('object');
   expect(params.sessionToken).to.equal(token);
@@ -46,6 +64,11 @@ const executeCommandEmulationFailed = sinon.spy((params) => {
 const closeSession = sinon.spy(() => Promise.resolve(true));
 
 // Service function
+const terminalServiceSlow = () => ({
+  getSessionToken,
+  executeCommand: executeCommandSlow,
+  closeSession,
+});
 const terminalServiceOk = () => ({
   getSessionToken,
   executeCommand: executeCommandOk,
@@ -55,6 +78,9 @@ const terminalServiceEmulationFailed = () => ({
   getSessionToken,
   executeCommand: executeCommandEmulationFailed,
   closeSession,
+});
+const terminalSlow = proxyquire('../../src/Services/Terminal/Terminal', {
+  './TerminalService': terminalServiceSlow,
 });
 const terminalOk = proxyquire('../../src/Services/Terminal/Terminal', {
   './TerminalService': terminalServiceOk,
@@ -66,13 +92,83 @@ const terminalEmulationFailed = proxyquire('../../src/Services/Terminal/Terminal
 // Tests
 describe('#Terminal', function terminalTest() {
   this.timeout(5000);
-  describe('Working with token', () => {
+  describe('Working with states', () => {
+    it('Should return error when executing command on closed terminal', () => {
+      // Resetting spies
+      getSessionToken.reset();
+      executeCommandOk.reset();
+      closeSession.reset();
+
+      const uAPITerminal = terminalOk({
+        auth: config,
+      });
+
+      return uAPITerminal
+        .closeSession()
+        .then(() => uAPITerminal.executeCommand('I'))
+        .then(() => Promise.reject(new Error('Command has been executed on closed terminal')))
+        .catch((err) => {
+          expect(closeSession.callCount).to.equal(1);
+          expect(getSessionToken.callCount).to.equal(1);
+          expect(executeCommandOk.callCount).to.equal(0);
+          expect(err).to.be.an.instanceof(
+            TerminalError.TerminalRuntimeError.TerminalIsClosed
+          );
+        });
+    });
+    it('Should return error when executing command on busy terminal', () => {
+      // Resetting spies
+      getSessionToken.reset();
+      executeCommandSlow.reset();
+
+      const uAPITerminal = terminalSlow({
+        auth: config,
+      });
+
+      return uAPITerminal
+        .executeCommand('I')
+        .then(() => Promise.all(['I', 'I'].map(command => uAPITerminal.executeCommand(command))))
+        .then(() => Promise.reject(new Error('Command has been executed on busy terminal')))
+        .catch((err) => {
+          expect(getSessionToken.callCount).to.equal(1);
+          expect(executeCommandSlow.callCount).to.equal(2);
+          expect(err).to.be.an.instanceof(
+            TerminalError.TerminalRuntimeError.TerminalIsBusy
+          );
+        });
+    });
+  });
+  describe('Executing commands', () => {
+    it('Should execute command', () => {
+      // Resetting spies
+      getSessionToken.reset();
+      executeCommandOk.reset();
+      closeSession.reset();
+
+      const uAPITerminal = terminalOk({
+        auth: config,
+      });
+
+      return uAPITerminal
+        .executeCommand('I')
+        .then((response) => {
+          expect(response).to.equal('IGNORED');
+        })
+        .then(() => uAPITerminal.closeSession())
+        .then(() => {
+          expect(getSessionToken.callCount).to.equal(1);
+          expect(executeCommandOk.callCount).to.equal(1);
+          expect(closeSession.callCount).to.equal(1);
+          expect(executeCommandOk.getCall(0).args[0].sessionToken).to.equal(token);
+          expect(executeCommandOk.getCall(0).args[0].command).to.equal('I');
+        });
+    });
+  });
+  describe('Working with emulation', () => {
     it('Should fail if emulation failed', () => {
       // Resetting spies
       getSessionToken.reset();
       executeCommandEmulationFailed.reset();
-      executeCommandOk.reset();
-      closeSession.reset();
 
       const emulatePcc = '7j8i';
       const emulateConfig = Object.assign({}, config, {
@@ -96,35 +192,9 @@ describe('#Terminal', function terminalTest() {
           );
         });
     });
-    it('Should execute command', () => {
-      // Resetting spies
-      getSessionToken.reset();
-      executeCommandEmulationFailed.reset();
-      executeCommandOk.reset();
-      closeSession.reset();
-
-      const uAPITerminal = terminalOk({
-        auth: config,
-      });
-
-      return uAPITerminal
-        .executeCommand('I')
-        .then((response) => {
-          expect(response).to.equal('IGNORED');
-        })
-        .then(() => uAPITerminal.closeSession())
-        .then(() => {
-          expect(getSessionToken.callCount).to.equal(1);
-          expect(executeCommandOk.callCount).to.equal(1);
-          expect(closeSession.callCount).to.equal(1);
-          expect(executeCommandOk.getCall(0).args[0].sessionToken).to.equal(token);
-          expect(executeCommandOk.getCall(0).args[0].command).to.equal('I');
-        });
-    });
     it('Should emulate pcc', () => {
       // Resetting spies
       getSessionToken.reset();
-      executeCommandEmulationFailed.reset();
       executeCommandOk.reset();
       closeSession.reset();
 
