54c368d56301826932597c49a010ed32c226bd04
Dmitry Chertousov <d.chertousov@gmail.com>
2017-02-10 12:19:05 +0100

    Added test for emulation fail

--- test/Terminal/Terminal.test.js
+++ test/Terminal/Terminal.test.js
@@ -6,13 +6,16 @@ import sinon from 'sinon';
 import { expect } from 'chai';
 import proxyquire from 'proxyquire';
 import config from '../testconfig';
+import uAPI from '../../src';
+
+const TerminalError = uAPI.errors.Terminal;
 
 // Token const
 const token = 'TOKEN';
 
 // Spied functions
 const getSessionToken = sinon.spy(() => Promise.resolve(token));
-const executeCommand = sinon.spy((params) => {
+const executeCommandOk = sinon.spy((params) => {
   expect(params).to.be.an('object');
   expect(params.sessionToken).to.equal(token);
   expect(params.command).to.be.a('string');
@@ -26,29 +29,81 @@ const executeCommand = sinon.spy((params) => {
       return Promise.resolve(['ERR']);
   }
 });
+const executeCommandEmulationFailed = sinon.spy((params) => {
+  expect(params).to.be.an('object');
+  expect(params.sessionToken).to.equal(token);
+  expect(params.command).to.be.a('string');
+  switch (params.command) {
+    case 'I':
+      return Promise.resolve(['IGNORED']);
+    default:
+      if (params.command.match(/^SEM/)) {
+        return Promise.resolve(['RESTRICTED']);
+      }
+      return Promise.resolve(['ERR']);
+  }
+});
 const closeSession = sinon.spy(() => Promise.resolve(true));
 
 // Service function
-const terminalService = () => ({
+const terminalServiceOk = () => ({
   getSessionToken,
-  executeCommand,
+  executeCommand: executeCommandOk,
   closeSession,
 });
-const terminal = proxyquire('../../src/Services/Terminal/Terminal', {
-  './TerminalService': terminalService,
+const terminalServiceEmulationFailed = () => ({
+  getSessionToken,
+  executeCommand: executeCommandEmulationFailed,
+  closeSession,
+});
+const terminalOk = proxyquire('../../src/Services/Terminal/Terminal', {
+  './TerminalService': terminalServiceOk,
+});
+const terminalEmulationFailed = proxyquire('../../src/Services/Terminal/Terminal', {
+  './TerminalService': terminalServiceEmulationFailed,
 });
 
 // Tests
 describe('#Terminal', function terminalTest() {
   this.timeout(5000);
   describe('Working with token', () => {
+    it('Should fail if emulation failed', () => {
+      // Resetting spies
+      getSessionToken.reset();
+      executeCommandEmulationFailed.reset();
+      executeCommandOk.reset();
+      closeSession.reset();
+
+      const emulatePcc = '7j8i';
+      const emulateConfig = Object.assign({}, config, {
+        emulatePcc,
+      });
+      const uAPITerminal = terminalEmulationFailed({
+        auth: emulateConfig,
+        emulatePcc,
+      });
+
+      return uAPITerminal
+        .executeCommand('I')
+        .then(() => Promise.reject(new Error('Emulation has not failed')))
+        .catch((err) => {
+          expect(getSessionToken.callCount).to.equal(1);
+          expect(executeCommandEmulationFailed.callCount).to.equal(1);
+          expect(executeCommandEmulationFailed.getCall(0).args[0].sessionToken).to.equal(token);
+          expect(executeCommandEmulationFailed.getCall(0).args[0].command).to.equal(`SEM/${emulatePcc}/AG`);
+          expect(err).to.be.an.instanceof(
+            TerminalError.TerminalRuntimeError.TerminalEmulationFailed
+          );
+        });
+    });
     it('Should execute command', () => {
       // Resetting spies
       getSessionToken.reset();
-      executeCommand.reset();
+      executeCommandEmulationFailed.reset();
+      executeCommandOk.reset();
       closeSession.reset();
 
-      const uAPITerminal = terminal({
+      const uAPITerminal = terminalOk({
         auth: config,
       });
 
@@ -60,23 +115,24 @@ describe('#Terminal', function terminalTest() {
         .then(() => uAPITerminal.closeSession())
         .then(() => {
           expect(getSessionToken.callCount).to.equal(1);
-          expect(executeCommand.callCount).to.equal(1);
+          expect(executeCommandOk.callCount).to.equal(1);
           expect(closeSession.callCount).to.equal(1);
-          expect(executeCommand.getCall(0).args[0].sessionToken).to.equal(token);
-          expect(executeCommand.getCall(0).args[0].command).to.equal('I');
+          expect(executeCommandOk.getCall(0).args[0].sessionToken).to.equal(token);
+          expect(executeCommandOk.getCall(0).args[0].command).to.equal('I');
         });
     });
     it('Should emulate pcc', () => {
       // Resetting spies
       getSessionToken.reset();
-      executeCommand.reset();
+      executeCommandEmulationFailed.reset();
+      executeCommandOk.reset();
       closeSession.reset();
 
       const emulatePcc = '7j8i';
       const emulateConfig = Object.assign({}, config, {
         emulatePcc,
       });
-      const uAPITerminal = terminal({
+      const uAPITerminal = terminalOk({
         auth: emulateConfig,
         emulatePcc,
       });
@@ -89,11 +145,11 @@ describe('#Terminal', function terminalTest() {
         .then(() => uAPITerminal.closeSession())
         .then(() => {
           expect(getSessionToken.callCount).to.equal(1);
-          expect(executeCommand.callCount).to.equal(2);
+          expect(executeCommandOk.callCount).to.equal(2);
           expect(closeSession.callCount).to.equal(1);
-          expect(executeCommand.getCall(0).args[0].sessionToken).to.equal(token);
-          expect(executeCommand.getCall(0).args[0].command).to.equal(`SEM/${emulatePcc}/AG`);
-          expect(executeCommand.getCall(1).args[0].command).to.equal('I');
+          expect(executeCommandOk.getCall(0).args[0].sessionToken).to.equal(token);
+          expect(executeCommandOk.getCall(0).args[0].command).to.equal(`SEM/${emulatePcc}/AG`);
+          expect(executeCommandOk.getCall(1).args[0].command).to.equal('I');
         });
     });
   });
