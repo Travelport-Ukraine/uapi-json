2fd5ffdd089f0c17fcea6e67e8172672d6ddd134
Dmitry Chertousov <d.chertousov@gmail.com>
2017-02-27 15:27:25 +0100

    More coverage added

--- test/Request/Request.test.js
+++ test/Request/Request.test.js
@@ -20,8 +20,8 @@ const serviceParams = [
   `${templatesDir}/AIR_LOW_FARE_SEARCH_REQUEST.xml`,
   null,
   () => ({}),
-  (x) => {console.log(x)},
-  (x) => {console.log(x)},
+  () => ({}),
+  () => 'PARSED',
 ];
 
 const requestError = proxyquire('../../src/Request/uapi-request', {
@@ -34,30 +34,40 @@ const requestJsonResponse = proxyquire('../../src/Request/uapi-request', {
     request: () => Promise.resolve({ data: '{"error": "Some error"}' }),
   },
 });
+const requestXMLResponse = proxyquire('../../src/Request/uapi-request', {
+  axios: {
+    request: () => Promise.resolve({ data: '<?xml version="1.0" encoding="UTF-8"?><SOAP:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><SOAP:Body><xml>Some xml</xml></SOAP:Body></SOAP:Envelope>' }),
+  },
+});
 
 describe('#Request', () => {
   describe('Request send', () => {
+    beforeEach(() => sinon.spy(console, 'log'));
+    afterEach(() => console.log.restore());
+
     it('should throw an SoapRequestError with underlying caused by Error', () => {
-      const request = requestError(...serviceParams);
+      const request = requestError(...serviceParams.concat(3));
       return request({})
         .catch((err) => {
           expect(err).to.be.an.instanceof(RequestSoapError.SoapRequestError);
           expect(err.causedBy).to.be.an.instanceof(Error);
+          expect(console.log).to.have.callCount(5);
         });
     });
     it('should throw SoapServerError when JSON received', () => {
-      const request = requestJsonResponse(...serviceParams);
+      const request = requestJsonResponse(...serviceParams.concat(3));
       return request({})
         .catch((err) => {
           expect(err).to.be.an.instanceof(RequestSoapError.SoapServerError);
+          expect(console.log).to.have.callCount(5);
         });
     });
-    it('should log debug info', () => {
-      sinon.spy(console, 'log');
-      const request = requestJsonResponse(...serviceParams.concat(2));
+    it('should call XML parse when XML received', () => {
+      const request = requestXMLResponse(...serviceParams.concat(3));
       return request({})
-        .catch(() => {
-          expect(console.log).to.have.callCount(4);
+        .then((response) => {
+          expect(response).to.equal('PARSED');
+          expect(console.log).to.have.callCount(7);
         });
     });
   });
