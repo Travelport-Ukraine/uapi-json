8d5184058446f5b46723e00168c801ecf4b428bb
Dmitry Chertousov <d.chertousov@gmail.com>
2017-03-06 12:36:36 +0100

    Fix for setGroupsForSegments. Tests for setGroupsForSegments. Added .test to tests

--- src/Services/Air/transformers/set-groups-for-segments.js
+++ src/Services/Air/transformers/set-groups-for-segments.js
@@ -1,6 +1,6 @@
 export default (params) => {
   params.segments = params.segments.map((segment, i, segments) => {
-    const transfer = (segments[i + 1] && segments[i + 1].group === segment.group);
+    const transfer = !!(segments[i + 1] && segments[i + 1].group === segment.group);
     return Object.assign(segment, { transfer });
   });
   return params;
---
+++ test/Air/transformers/set-groups-for-segments.test.js
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+
+import convert from '../../../src/Services/Air/transformers/set-groups-for-segments';
+
+describe('Air.validators.setGroupsForSegments', () => {
+  it('should add transfer info for segments', () => {
+    const params = {
+      segments: [
+        { group: 0 },
+        { group: 0 },
+        { group: 1 },
+        { group: 2 },
+      ],
+    };
+    const converted = convert(params);
+    console.log(converted);
+    process.exit();
+    expect(converted.segments[0].transfer).to.be.equal(true);
+    expect(converted.segments[1].transfer).to.be.equal(false);
+    expect(converted.segments[2].transfer).to.be.equal(false);
+    expect(converted.segments[3].transfer).to.be.equal(false);
+  });
+});
+
