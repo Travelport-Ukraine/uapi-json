4ba315fd7a47292d98c7fa770b82dab3285d6ecb
Dmitry Chertousov <d.chertousov@gmail.com>
2017-03-06 14:31:03 +0100

    Better coverage for phone validator (could be yet improved).

---
+++ test/Air/validators/phone.test.js
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+import { AirValidationError } from '../../../src/Services/Air/AirErrors';
+
+import validate from '../../../src/Services/Air/validators/phone';
+
+describe('Air.validators.phoneOptional', () => {
+  it('should throw error when no phone is set', () => {
+    const options = {};
+    const fn = () => validate(options);
+    expect(fn).to.throw(AirValidationError.PhoneMissing);
+  });
+  it('should throw error when no phone requeired fields are set', () => {
+    const options = { phone: {} };
+    const fn = () => validate(options);
+    expect(fn).to.throw(AirValidationError.IncorrectPhoneFormat);
+  });
+  it('should pass if all phone fields are set', () => {
+    const options = {
+      phone: { number: 123, countryCode: 'UA', location: 'IEV' },
+    };
+    const fn = () => validate(options);
+    expect(fn).not.to.throw(Error);
+  });
+});
