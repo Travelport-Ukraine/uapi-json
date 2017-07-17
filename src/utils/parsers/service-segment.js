041849aa92bccee0c5771381cc27510060090eb8
Dmitry Chertousov <d.chertousov@travelport.ua>
2017-07-17 20:01:10 +0300

    Fixed parsing of service segments when issued

--- src/utils/parsers/service-segment.js
+++ src/utils/parsers/service-segment.js
@@ -2,7 +2,7 @@ const itemPattern = new RegExp([
   // rfiCode, rfiSubcode
   '-([A-Z]{1})/([A-Z0-9]{3})',
   // Fee description, passenger name, amount, currency
-  '\\/([^\\/]+)/NM-1(.+?)\\/\\/((?:\\d+\\.)?\\d+)\\/([A-Z]{3})',
+  '\\/([^\\/]+)/NM-1(.+?)\\/([^\\/]*)\\/((?:\\d+\\.)?\\d+)\\/([A-Z]{3})',
 ].join(''), 'i');
 
 const parse = (string) => {
@@ -12,18 +12,24 @@ const parse = (string) => {
     rfiSubcode,
     feeDescription,
     name,
+    documentNumber,
     amount,
     currency,
   ] = string.match(itemPattern);
 
-  return {
-    rfiCode,
-    rfiSubcode,
-    feeDescription,
-    name,
-    amount: Number(amount),
-    currency,
-  };
+  return Object.assign(
+    {
+      rfiCode,
+      rfiSubcode,
+      feeDescription,
+      name,
+      amount: Number(amount),
+      currency,
+    },
+    documentNumber !== ''
+      ? { ticketNumber: documentNumber.substr(0, 13) }
+      : null
+  );
 };
 
 export default parse;
