4b7acc44a0552c05c9816fee1bf1eecce16a4629
Artem Pylypchuk <articicejuice@gmail.com>
2017-08-23 20:09:29 +0300

    #266 revert AIR_FARE_RULES_REQUEST

--- src/Services/Air/templates/AIR_FARE_RULES_REQUEST.handlebars.js
+++ src/Services/Air/templates/AIR_FARE_RULES_REQUEST.handlebars.js
@@ -2,43 +2,21 @@ module.exports = `
 <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
     <soapenv:Header/>
     <soapenv:Body>
-        <air:AirPriceReq
-            AuthorizedBy="user" TraceId="{{requestId}}" TargetBranch="{{TargetBranch}}"
-            CheckOBFees="false"
-            FareRuleType="{{#if long}}long{{else}}short{{/if}}"
+        <air:AirFareRulesReq
+            TraceId="test" AuthorizedBy="{{requestId}}" TargetBranch="{{TargetBranch}}"
+            RetrieveProviderReservationDetails="false" FareRuleType="long"
             xmlns:air="http://www.travelport.com/schema/air_v36_0"
-            xmlns:com="http://www.travelport.com/schema/common_v36_0">
-            <com:BillingPointOfSaleInfo OriginApplication="uAPI"/>
-            <air:AirItinerary>
-                {{#segments}}
-                <air:AirSegment Key="{{@index}}" Group="0" ProviderCode="1G"
-                                Carrier="{{airline}}"
-                                CabinClass="{{serviceClass}}"
-                                FlightNumber="{{flightNumber}}"
-                                Origin="{{from}}"
-                                Destination="{{to}}"
-                                DepartureTime="{{departure}}"
-                                ArrivalTime="{{arrival}}"
-                                {{#if duration}}TravelTime="{{duration}}"{{/if}}
-                                ClassOfService="{{bookingClass}}"
-                                TravelOrder="{{@index}}" />
-                    {{#if transfer}}
-                    <air:Connection/>
-                    {{/if}}
-                {{/segments}}
-            </air:AirItinerary>
-            <com:SearchPassenger Key="P_0" Code="ADT" />
-            {{#if hasFareBasis}}
-            <air:AirPricingCommand>
-                {{#segments}}
-                <air:AirSegmentPricingModifiers AirSegmentRef="{{@index}}" FareBasisCode="{{fareBasisCode}}"/>
-                {{/segments}}
-            </air:AirPricingCommand>
-            {{/if}}
+            xmlns:com="http://www.travelport.com/schema/common_v36_0"
+            >
+            <com:BillingPointOfSaleInfo OriginApplication="uAPI" xmlns:com="http://www.travelport.com/schema/common_v36_0"/>
+            <air:FareRuleKey FareInfoRef="0" ProviderCode="1G">{{uapi_fare_rule_key}}</air:FareRuleKey>
+            {{#each provider_cmds}}
+            <air:AirFareDisplayRuleKey ProviderCode="1G">{{this}}</air:AirFareDisplayRuleKey>
+            {{/each}}
             {{#if emulatePcc}}
             <com:OverridePCC ProviderCode="1G" PseudoCityCode="{{emulatePcc}}"/>
             {{/if}}
-        </air:AirPriceReq>
+        </air:AirFareRulesReq>
     </soapenv:Body>
 </soapenv:Envelope>
 `;
