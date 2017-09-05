9bbfd70c136ae3829fb7f6e1b18d6003a8a44c59
Artem Pylypchuk <articicejuice@gmail.com>
2017-09-05 08:28:10 +0300

    Add interactive terminal example

---
+++ examples/Terminal/interactive.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+require('babel-register');
+
+const readline = require('readline2');
+const prefix = '>';
+const uAPI = require('../../index');
+const testConfig = require('../../test/testconfig');
+
+const config = Object.assign({}, testConfig);
+
+const TerminalService = uAPI.createTerminalService({
+  auth: config,
+  debug: 0,
+  production: true,
+});
+
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+const prompt = () => {
+  rl.setPrompt(prefix, prefix.length);
+  rl.prompt();
+};
+
+prompt();
+rl.on('line', (line) => {
+  // TerminalService doesn't accept empty commands
+  if (!line) {
+    console.log('CHECK ACTION CODE');
+    prompt();
+    return;
+  }
+
+  rl.pause();
+  TerminalService
+  .executeCommand(line)
+    .then((reply) => {
+      process.stdout.write(reply.slice(0, -1));
+    })
+    .then(() => {
+      rl.resume();
+    }).catch((err) => {
+      console.error(err);
+    });
+}).on('close', () => {
+  console.log('Disconnecting');
+  TerminalService.closeSession()
+    .then(() => {
+      process.exit(0);
+    });
+});
