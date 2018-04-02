4c9b99344ec629de5d39e5a4b1f594ca9ceb6c04
Mark Orel <mail.ormark@gmail.com>
2018-04-02 22:22:15 +0300

    Added one more test with random string.

--- test/utils.test.js
+++ test/utils.test.js
@@ -280,6 +280,10 @@ describe('#Utils', () => {
         expect(utils.getErrorPcc(null)).to.be.a('null');
       });
 
+      it('should not break on any other string', () => {
+        expect(utils.getErrorPcc('123')).to.equal(null);
+      });
+
       it('should work with correct data', () => {
         expect(utils.getErrorPcc('NO AGREEMENT EXISTS FOR AGENCY - 38KP')).to.equal('38KP');
       });
