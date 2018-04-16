65f9ec3271569ed348982fdcd7212ef5446bfa8f
Dmitry Chertousov <d.chertousov@gmail.com>
2018-04-16 10:05:57 +0200

    Added md fix for uapi terminal issue

--- test/Terminal/Terminal.test.js
+++ test/Terminal/Terminal.test.js
@@ -2,6 +2,7 @@
   eslint-disable import/no-extraneous-dependencies
 */
 
+import fs from 'fs';
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import sinonChai from 'sinon-chai';
@@ -19,6 +20,24 @@ const ModifiedTerminalRuntimeError = Object.assign({}, TerminalRuntimeError, {
   ErrorClosingSession: DumbErrorClosingSession,
 });
 
+const wait = time => new Promise((resolve) => {
+  setTimeout(() => resolve(), time);
+});
+
+const getTerminalResponse = path => new Promise((resolve, reject) => {
+  fs.readFile(
+    `${__dirname}/TerminalResponses/${path}.txt`,
+    (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      const res = data.toString().replace(/\s*$/, '');
+      resolve(res.split(/\n/));
+    },
+  );
+});
+
 // Token const
 const token = 'TOKEN';
 
@@ -30,16 +49,13 @@ const executeCommandSlow = sinon.spy((params) => {
   expect(params.command).to.be.a('string');
   switch (params.command) {
     case 'I':
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          resolve(['IGNORED']);
-        }, 100);
-      });
+      return wait(100)
+        .then(() => getTerminalResponse('I'));
     default:
       if (params.command.match(/^SEM/)) {
-        return Promise.resolve(['PROCEED']);
+        return getTerminalResponse('SEM');
       }
-      return Promise.resolve(['ERR']);
+      return getTerminalResponse('ERR');
   }
 });
 const executeCommandOk = sinon.spy((params) => {
@@ -48,46 +64,41 @@ const executeCommandOk = sinon.spy((params) => {
   expect(params.command).to.be.a('string');
   switch (params.command) {
     case 'I':
-      return Promise.resolve(['IGNORED']);
+      return getTerminalResponse('I');
     case 'TE':
-      return Promise.resolve([
-        'TKT: 064 9902 789263     NAME: CHERTOUSOV/DMYTROMR             ',
-        '                                                               ',
-        'ISSUED: 11JAN17          FOP:CASH                              ',
-        'PSEUDO: 7J8J  PLATING CARRIER: OK  ISO: UA  IATA: 99999992     ',
-        '   USE  CR FLT  CLS  DATE BRDOFF TIME  ST F/B        FARE   CPN',
-        '   RFND OK  917  Q  15MAY KBPPRG  0505 OK Q0BAGG             1 ',
-        '                                          NVB15MAY NVA15MAY    ',
-        '   RFND OK  630  P  15MAY PRGBRU  0655 OK P0BAGG             2 ',
-        '                                          NVB15MAY NVA15MAY    ',
-        '                                                               ',
-        'FARE USD    41.00 TAX      109UA TAX       55UD TAX     2075XT ',
-        'TOTAL UAH     3357                                             ',
-        'EQUIV UAH     1118                                             ',
-        ')><',
-      ]);
+      return getTerminalResponse('set01/TE-P1');
     case 'MD':
-      return Promise.resolve([
-        '                                          NVB15MAY NVA15MAY    ',
-        '   RFND OK  630  P  15MAY PRGBRU  0655 OK P0BAGG             2 ',
-        '                                          NVB15MAY NVA15MAY    ',
-        '                                                               ',
-        'FARE USD    41.00 TAX      109UA TAX       55UD TAX     2075XT ',
-        'TOTAL UAH     3357                                             ',
-        'EQUIV UAH     1118                                             ',
-        '   FARE RESTRICTIONS APPLY                                     ',
-        '                                                               ',
-        'IEV OK PRG 39.00 OK BRU 2.17 NUC41.17END ROE1.0 XT             ',
-        '464YK231CZ1380YQ                                               ',
-        'RLOC 1G BGQF5K    1A ZWWJYF                                    ',
-        '                                                               ',
-        '><',
-      ]);
+      return getTerminalResponse('set01/TE-P2');
     default:
       if (params.command.match(/^SEM/)) {
-        return Promise.resolve(['PROCEED']);
+        return getTerminalResponse('SEM');
       }
-      return Promise.resolve(['ERR']);
+      return getTerminalResponse('ERR');
+  }
+});
+const executeCommandMdIssues = sinon.spy((params) => {
+  expect(params).to.be.an('object');
+  expect(params.sessionToken).to.equal(token);
+  expect(params.command).to.be.a('string');
+
+  const mdCall = sinon.stub();
+  mdCall.onCall(0).returns(
+    getTerminalResponse('set02/HFF-P2')
+  );
+  mdCall.onCall(1).returns(
+    getTerminalResponse('set02/HFF-P3')
+  );
+
+  switch (params.command) {
+    case '*HFF':
+      return getTerminalResponse('set02/HFF-P1');
+    case 'MD':
+      return mdCall();
+    default:
+      if (params.command.match(/^SEM/)) {
+        return getTerminalResponse('SEM');
+      }
+      return getTerminalResponse('ERR');
   }
 });
 const executeCommandEmulationFailed = sinon.spy((params) => {
@@ -96,12 +107,12 @@ const executeCommandEmulationFailed = sinon.spy((params) => {
   expect(params.command).to.be.a('string');
   switch (params.command) {
     case 'I':
-      return Promise.resolve(['IGNORED']);
+      return getTerminalResponse('I');
     default:
       if (params.command.match(/^SEM/)) {
-        return Promise.resolve(['RESTRICTED']);
+        return getTerminalResponse('RESTRICTED');
       }
-      return Promise.resolve(['ERR']);
+      return getTerminalResponse('ERR');
   }
 });
 const closeSession = sinon.spy(() => Promise.resolve(true));
@@ -116,6 +127,11 @@ const terminalServiceSlow = () => ({
 const terminalServiceOk = () => ({
   getSessionToken,
   executeCommand: executeCommandOk,
+  closeSession,
+});
+const terminalServiceMdIssues = () => ({
+  getSessionToken,
+  executeCommand: executeCommandMdIssues,
   closeSession,
 });
 const terminalServiceCloseSessionError = () => ({
@@ -139,6 +155,9 @@ const terminalSlow = proxyquire('../../src/Services/Terminal/Terminal', {
 });
 const terminalOk = proxyquire('../../src/Services/Terminal/Terminal', {
   './TerminalService': terminalServiceOk,
+});
+const terminalMdIssues = proxyquire('../../src/Services/Terminal/Terminal', {
+  './TerminalService': terminalServiceMdIssues,
 });
 const terminalEmulationFailed = proxyquire('../../src/Services/Terminal/Terminal', {
   './TerminalService': terminalServiceEmulationFailed,
@@ -331,6 +350,21 @@ describe('#Terminal', function terminalTest() {
           expect(executeCommandOk.getCall(0).args[0].sessionToken).to.equal(token);
           expect(executeCommandOk.getCall(0).args[0].command).to.equal('TE');
           expect(executeCommandOk.getCall(1).args[0].command).to.equal('MD');
+        });
+    });
+    it('should handle uapi MD issues', () => {
+      // Resetting spies
+      getSessionToken.reset();
+      executeCommandOk.reset();
+      closeSession.reset();
+
+      const uAPITerminal = terminalMdIssues({
+        auth: config,
+      });
+
+      return uAPITerminal.executeCommand('*HFF')
+        .then((response) => {
+          expect(response).to.equal('     *****      FILED FARE HISTORY          4PP13A    *****\nXFQ ZAINULLIN/IURIIMR-ADT  G  UAH    13013      03APR18\nAOB ZAINULLIN/IURIIMR-ADT  TTLUAH    13013      03APR18\nAOB ZAINULLIN/IURIIMR-ADT     UAH        0     03APR18\nAFQ ZAINULLIN/IURIIMR-ADT  G              7712625095845\nXFQ ZAINULLIN/IURIIMR-ADT  G  UAH    13013      03APR18\nAOB ZAINULLIN/IURIIMR-ADT  TTLUAH    13013      03APR18\nAOB ZAINULLIN/IURIIMR-ADT     UAH        0     03APR18\nAFQ ZAINULLIN/IURIIMR-ADT  G              7712625095845\nXFQ ZAINULLIN/IURIIMR-ADT  G  UAH    13013      03APR18\nAOB ZAINULLIN/IURIIMR-ADT  TTLUAH    13013      03APR18\nAOB ZAINULLIN/IURIIMR-ADT     UAH        0     03APR18\nAFQ ZAINULLIN/IURIIMR-ADT  G              7712625095845\nAFQ ZAINULLIN/IURIIMR-ADT  G              7712625095845\nXFQ ZAINULLIN/IURIIMR-ADT  G  UAH    13013      03APR18\nAOB ZAINULLIN/IURIIMR-ADT  TTLUAH    13013      03APR18\nAOB ZAINULLIN/IURIIMR-ADT     UAH        0     03APR18\nAFQ ZAINULLIN/IURIIMR-ADT  G              7712625095845\nXFQ ZAINULLIN/IURIIMR-ADT  G  UAH    13013      03APR18\nAOB ZAINULLIN/IURIIMR-ADT  TTLUAH    13013      03APR18\nAOB ZAINULLIN/IURIIMR-ADT     UAH        0     03APR18\nAFQ ZAINULLIN/IURIIMR-ADT  G              7712625095845\nRCVD-\nCRDT- IEV/5JV7/1G AG          0648Z/03APR\nXFQ ZAINULLIN/IURIIMR    I-ADT  I  UAH    12698      03APR18\n\n>XFQP1/-P*5JV7/TA7W30)><\n)><');
         });
     });
   });
