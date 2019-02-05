94dd62e25d22345ca3ed74e78bd340143ae88f23
Paul Voloschuk <pavelv@roundforest.com>
2019-02-05 12:22:06 +0200

    Test

---
+++ test/Air/validators/platingCarrier-optional.test.js
@@ -0,0 +1,16 @@
+const { expect } = require('chai');
+const { AirValidationError } = require('../../../src/Services/Air/AirErrors');
+
+const validate = require('../../../src/Services/Air/validators/platingCarrier');
+
+describe('Air.validators.platingCarrier', () => {
+  it('should pass', () => {
+    const platingCarrier = 'A1';
+    validate({ platingCarrier });
+  });
+
+  it('should throw error', () => {
+    const fn = () => validate({ platingCarrier: '=_=' });
+    expect(fn).to.throw(AirValidationError.PlatingCarrierInvalid);
+  });
+});
