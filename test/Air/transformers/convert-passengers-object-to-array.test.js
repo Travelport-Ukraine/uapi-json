8fbe5db1d1ea52516685fc107ef1e5229173c0ef
Ju Hyuk Park <jpark@roo.kr>
2019-06-25 14:37:35 +0900

    test fix

--- test/Air/transformers/convert-passengers-object-to-array.test.js
+++ test/Air/transformers/convert-passengers-object-to-array.test.js
@@ -8,9 +8,9 @@ describe('Air.transformers.convertPassengersObjectToArray', () => {
     const converted = convert({ passengers: { ADT: 1, CNN: 3, INF: 1 } });
     expect(converted).to.have.all.keys(['passengers']);
     converted.passengers.map((passenger) => {
-      expect(passenger).to.have.all.keys(['ageCategory', 'child']);
+      expect(passenger).to.have.all.keys(['ageCategory', 'isChild']);
       if (passenger.ageCategory === 'CNN') {
-        expect(passenger.child).to.be.true;
+        expect(passenger.isChild).to.be.true;
       }
 
       return null;
