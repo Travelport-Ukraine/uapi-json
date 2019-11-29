4c58d52e0de76c35a16522760d8e82f565bb2338
Ju Hyuk Park <jpark@roo.kr>
2019-11-29 13:35:46 +0900

    added test for transformer

--- src/Services/Air/transformers/set-segment-ref-for-ssr.js
+++ src/Services/Air/transformers/set-segment-ref-for-ssr.js
@@ -1,7 +1,7 @@
 module.exports = (params) => {
   params.passengers = params.passengers.map((passenger) => {
     passenger.ssr = (passenger.ssr || []).map((ssr) => {
-      if (ssr.segment) {
+      if (ssr.segment !== undefined) {
         const segKey = Object.keys(params['air:AirSegment'])[ssr.segment];
         ssr.segmentRef = params['air:AirSegment'][segKey].Key;
 
---
+++ test/Air/transformers/set-segment-ref-for-ssr.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+
+const convert = require('../../../src/Services/Air/transformers/set-segment-ref-for-ssr');
+
+describe('Air.transformers.setsSegmentRefForSSR', () => {
+  it('should set segmentRef from segment index', () => {
+    const params = {
+      'air:AirSegment': {
+        'DummySegmentRefA===': { Key: 'DummySegmentRefA===' },
+        'DummySegmentRefB===': { Key: 'DummySegmentRefB===' },
+      },
+      passengers: [
+        {
+          ssr: [
+            { type: 'FQTV', segment: 0 },
+            { type: 'FQTV', segment: 1 },
+          ]
+        },
+      ],
+    };
+    const converted = convert(params);
+    console.log(converted.passengers[0].ssr)
+    expect(converted.passengers[0].ssr[0].segmentRef).to.be.equal('DummySegmentRefA===');
+    expect(converted.passengers[0].ssr[1].segmentRef).to.be.equal('DummySegmentRefB===');
+  });
+});
