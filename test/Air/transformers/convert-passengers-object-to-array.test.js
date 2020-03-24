01b8016af9a2430da0c8b58ae6f482d837400f4d
Ju Hyuk Park <jpark@roo.kr>
2020-03-24 11:23:28 +0900

    This shouldn't be in this PR..

--- test/Air/transformers/convert-passengers-object-to-array.test.js
+++ test/Air/transformers/convert-passengers-object-to-array.test.js
@@ -8,7 +8,7 @@ describe('Air.transformers.convertPassengersObjectToArray', () => {
     const converted = convert({ passengers: { ADT: 1, CNN: 3, INF: 1 } });
     expect(converted).to.have.all.keys(['passengers']);
     converted.passengers.map((passenger) => {
-      expect(passenger).to.have.all.keys(['ageCategory', 'isChild']);
+      expect(passenger).to.have.all.keys(['ageCategory', 'child']);
       if (passenger.ageCategory === 'CNN') {
         expect(passenger.isChild).to.be.true;
       }
