10e98149ccc6d5ff0de1235afc3a582a2bd7ff85
Ju Hyuk Park <jpark@roo.kr>
2020-03-24 11:19:23 +0900

    No human could see this could be an error? -- retyped the file name

--- src/Services/Air/templates/index.js
+++ src/Services/Air/templates/index.js
@@ -15,7 +15,7 @@ const universalRecordCancelUr = require('./UNIVERSAL_RECORD_CANCEL_UR.handlebars
 const universalRecordFoid = require('./UNIVERSAL_RECORD_FOID.handlebars');
 const universalRecordImport = require('./UNIVERSAL_RECORD_IMPORT_REQUEST.handlebars');
 const universalRecordRetrieve = require('./UNIVERSAL_RECORD_RETRIEVE_REQUEST.handlebars');
-const availability = require('./AIR_AVAILABILTIY_REQUEST.handlebars');
+const availability = require('./AIR_AVAILABILITY_REQUEST.handlebars');
 
 module.exports = {
   createReservation,
