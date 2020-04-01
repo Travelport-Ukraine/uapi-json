b8561892c92982cb26dc5722619809e9a71f95af
Artem Barbyniahra <xaputo@gmail.com>
2020-04-01 05:01:42 +0300

    Add some more coverage

--- test/utils-helpers/unescape-html.test.js
+++ test/utils-helpers/unescape-html.test.js
@@ -1,10 +1,20 @@
 const { expect } = require('chai');
 const unescapeHtml = require('../../src/utils/unescape-html');
 
-describe('#unescapeHtml', () => {
+describe.only('#unescapeHtml', () => {
   it('should properly transform lt, newline and gt', () => {
     const source = 'UNABLE TO RETRIEVE - RESTRICTED BF   \n&gt;&lt;';
     expect(unescapeHtml(source)).to.be.eq('UNABLE TO RETRIEVE - RESTRICTED BF   \n><');
+  });
+
+  it('should properly transform quot', () => {
+    const source = 'sumtest &quot;';
+    expect(unescapeHtml(source)).to.be.eq('sumtest "');
+  });
+
+  it('should properly transform nbsp', () => {
+    const source = 'some text&nbsp;';
+    expect(unescapeHtml(source)).to.be.eq('some text ');
   });
 
   it('should not change original string', () => {
